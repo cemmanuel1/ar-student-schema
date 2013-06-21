961d17b8875d5c7c5819444a14b595be467fa464
Apprentice <apprentice@dbc25.local>
2013-06-20 18:46:59 -0700

    added email validation to teachers

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,4 +2,14 @@ require_relative '../../db/config'
 
 class Teacher < ActiveRecord::Base
 
+  validates :email, :uniqueness => true
+
 end
+
+Teacher.create(:name => "Laura", :email => "fiona.dickens@erdman.org", :address => "Loin", :phone => "123")
+
+  #   @teacher = Teacher.new
+  #   @teacher.assign_attributes(
+  #      :name => "Laura"email: => "fiona.diens@erdman.org", :address => "Loin", :phone => "123"
+  #   )
+  # end
